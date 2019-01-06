d0bb9ef4b06e039088419cab83474e5febf88b8f
Caleb Bassham <caleb.bassham@gmail.com>
2019-01-06 16:49:32 -0600

    Add rank prefix

--- src/main/java/me/calebbassham/servermessages/ServerMessages.java
+++ src/main/java/me/calebbassham/servermessages/ServerMessages.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -23,6 +24,19 @@ public class ServerMessages extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent e) {
         e.setQuitMessage(cc(String.format("&8[&c-&8] &3%s &8&o(&b&o%d&8&o/&3&o%d&8&o)", e.getPlayer().getDisplayName(), Bukkit.getOnlinePlayers().size() - 1, Bukkit.getMaxPlayers())));
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        var player = e.getPlayer();
+        if (player.hasPermission("riptide.host")) {
+            player.setDisplayName(cc(String.format("&8[&bHost&8] &3%s", player.getName())));
+        }
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        e.setFormat(cc("&b%s&7: &f%s"));
     }
 
     private String cc(String message) {
